1564c21d18554c7b8afb6afc9178b7a5a2761df4
kuvarviren <kuvarviren@gmail.com>
2022-03-21 15:51:55 +0530

    Create project using Create-React-App

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Hello(){
+  return <h1>Welcome to React!</h1>
+}
+ReactDOM.render(
+  <Hello/>,
+  document.getElementById('root')
+);
+
