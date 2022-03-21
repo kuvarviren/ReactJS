19bfc73f418d354d84b778768a371e9c5021e584
kuvarviren <kuvarviren@gmail.com>
2022-03-21 17:43:21 +0530

    Using React Fragement

--- src/index.js
+++ src/index.js
@@ -2,27 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const lakeList = [
-  {id:1,name:"Virthur", trailhead:"Echo"},
-  {id:2,name:"BTM",trailhead:"BayView"},
-  {id:3,name:"Thippasandra",trailhead:"OceanView"},
- ];
-
+function Lake(){
+  return <h1>Lake!</h1>;
+}
+function Ski(){
+  return <h1>Ski!</h1>;
+}
 function App({lakes}){
   return (
-    <div>
-      {
-        lakes.map(lake=>(
-          <div key={lake.id}>
-            <h1>{lake.name}</h1>
-            <p>{lake.trailhead}</p>
-          </div>))
-      }
-    </div>
+    <React.Fragment>
+      <Lake />
+      <Ski />
+    </React.Fragment>
   );
 }
 ReactDOM.render(
-  <App lakes = {lakeList} />,
+  <App />,
   document.getElementById('root')
 );
 
