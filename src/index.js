f41358646b061288b32d6b16cb81d48a014d5370
kuvarviren <kuvarviren@gmail.com>
2022-03-23 14:46:51 +0530

    dependency array, the second argument of useEffect hook which works in conjuction of other hook

--- src/index.js
+++ src/index.js
@@ -2,18 +2,32 @@ import React,{useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Checkbox(){
-  const [checked,setCheck] = useState(false);
+function App(){
+  const [val,setval] = useState("");
+  const [val2,setval2] = useState("");
   //To print the value of checked state
-  useEffect = alert(`The value of checked: ${checked.toString()}`);
+  useEffect(()=>{
+    console.log(`field1: ${val}`);
+  },[val]);
+  useEffect(()=>{
+    console.log(`field2: ${val2}`);
+  },[val2]);
   return (
     <>
-     <input type="checkbox" value={checked} onChange={()=>setCheck(checked=>!checked)}/> {checked ? "Agree" : "Disagree"}
+      <label>
+        favorite phrase:
+        <input value={val} onChange={e=>setval(e.target.value)}/>
+      </label>
+      <br />
+      <label>
+        other favorite phrase:
+        <input value={val2} onChange={e=>setval2(e.target.value)}/>
+      </label>
     </>
   );
 }
 ReactDOM.render(
-  <Checkbox />,
+  <App />,
   document.getElementById('root')
 );
 
