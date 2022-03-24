782101a077728e602a3e1597ec25b798715e9a07
kuvarviren <kuvarviren@gmail.com>
2022-03-24 17:09:17 +0530

    useReducer Hook is used for complex state management in application

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-import React,{useState, useEffect} from 'react';
+import React,{useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App(){
-  const [val,setval] = useState("");
-  const [val2,setval2] = useState("");
-  //To print the value of checked state
-  useEffect(()=>{
-    console.log(`field1: ${val}`);
-  },[val]);
-  useEffect(()=>{
-    console.log(`field2: ${val2}`);
-  },[val2]);
+function Checkbox(){
+  const [checked,toggle] = useReducer(checked=>!checked,false);
   return (
     <>
-      <label>
-        favorite phrase:
-        <input value={val} onChange={e=>setval(e.target.value)}/>
-      </label>
-      <br />
-      <label>
-        other favorite phrase:
-        <input value={val2} onChange={e=>setval2(e.target.value)}/>
-      </label>
+     <input type="checkbox" value={checked} onChange={toggle}/> {checked ? "Agree" : "Disagree"}
     </>
   );
 }
 ReactDOM.render(
-  <App />,
+  <Checkbox />,
   document.getElementById('root')
 );
-
 
